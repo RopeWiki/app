7a4ad03a3cf248e67855372e8d5f72f3a6c37edb
Benjamin Pelletier <pelletierb@wing.com>
2021-07-19 19:24:35 -0700

    Update docker-compose for starting prod

--- production/start_prod.sh
+++ production/start_prod.sh
@@ -6,4 +6,9 @@
 SQL_BACKUP_FOLDER=/rw/mount/sqlbackup
 IMAGES_FOLDER=/rw/mount/images
 
-docker-compose -d up
+if [[ -z "${WG_DB_PASSWORD}" ]]
+then
+  echo Warning: environment variable WG_DB_PASSWORD not set; a restored database will probably not work.
+fi
+
+docker-compose up -d
