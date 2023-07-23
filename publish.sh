8ddeaa820d97c6218406b01d488a719dae2fe83b
hcooper <coops@fawk.eu>
2023-07-23 12:33:38 -0700

    Improve `publish.sh` (#51)

--- publish.sh
+++ publish.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-RW_SITE_VERSION=${1:?A version like v1.0.0 must be specified for this script}
-docker tag ropewiki/reverse_proxy:latest ropewiki/reverse_proxy:${RW_SITE_VERSION}
-docker push ropewiki/reverse_proxy:${RW_SITE_VERSION}
-docker push ropewiki/reverse_proxy:latest
-docker tag ropewiki/webserver:latest ropewiki/webserver:${RW_SITE_VERSION}
-docker push ropewiki/webserver:${RW_SITE_VERSION}
-docker push ropewiki/webserver:latest
-docker tag ropewiki/database:latest ropewiki/database:${RW_SITE_VERSION}
-docker push ropewiki/database:${RW_SITE_VERSION}
-docker push ropewiki/database:latest
-docker tag ropewiki/backup_manager:latest ropewiki/backup_manager:${RW_SITE_VERSION}
-docker push ropewiki/backup_manager:${RW_SITE_VERSION}
-docker push ropewiki/backup_manager:latest
-docker tag ropewiki/mailserver:latest ropewiki/mailserver:${RW_SITE_VERSION}
-docker push ropewiki/mailserver:${RW_SITE_VERSION}
-docker push ropewiki/mailserver:latest
-echo "Remember to create and push the git tag {$RW_SITE_VERSION}"
+# This script is to be ran after a successful deployment of the ropewiki stack.
+# It tags docker images and github commits with versions numbers which can be used in future as known "good versions".
+
+RW_SITE_VERSION=${1:?This script expects a version tag as the first argument. Run "git tag" to see current version tags.}
+
+images=(
+    "backup_manager"
+    "database"
+    "mailserver"
+    "reverse_proxy"
+    "webserver"
+)
+
+for image in "${images[@]}"; do
+    docker tag ropewiki/$image:latest ropewiki/$image:${RW_SITE_VERSION}
+    docker push ropewiki/$image:${RW_SITE_VERSION}
+    docker push ropewiki/$image:latest
+done
+
+echo
+echo "########################"
+echo
+echo "Remember to create and push the git tag $RW_SITE_VERSION. This needs to be done from a checkout with write access to github."
+echo "  git checkout $(git rev-parse HEAD)"
+echo "  git tag ${RW_SITE_VERSION}"
+echo "  git push origin ${RW_SITE_VERSION}"
+echo
+echo "########################"
+echo
