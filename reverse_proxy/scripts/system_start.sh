e42d5367983bd459fe8b9f6cb6e2abedece550c3
hcooper <coops@fawk.eu>
2023-08-09 18:03:52 -0700

    nginx log changes & parsing (#69)

--- reverse_proxy/scripts/system_start.sh
+++ reverse_proxy/scripts/system_start.sh
@@ -8,10 +8,26 @@ else
   echo "Configuring nginx services..."
   sed -i "s/{{WG_HOSTNAME}}/$WG_HOSTNAME/g" /etc/nginx/services.conf
 
+  mkdir -p /logs/goaccess/db
+  mkdir -p /logs/nginx
+  touch /logs/nginx/access.log
   touch /ropewiki_setup_complete
 
   echo "<<< RopeWiki reverse proxy prep complete."
 fi
+
+echo "Starting up goaccess log parsing..."
+goaccess /logs/nginx/access.log \
+  -o /logs/goaccess/report.html \
+  --log-format COMBINED \
+  --real-time-html \
+  --ws-url wss://$WG_HOSTNAME:443/reportws \
+  --daemonize \
+  --db-path /logs/goaccess/db \
+  --restore \
+  --persist \
+  --html-refresh 10 \
+  --html-report-title "Ropewiki Stats"
 
 echo "RopeWiki reverse proxy running nginx..."
 nginx -g "daemon off;"
