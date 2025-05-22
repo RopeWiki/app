0f96f1235ed19c96c160590379c283f86acf9a7f
hcooper <coops@fawk.eu>
2025-05-22 11:57:57 -0700

    enable InputBox (#149)

--- webserver/html/ropewiki/LocalSettings.php
+++ webserver/html/ropewiki/LocalSettings.php
@@ -236,6 +236,7 @@ $smwgNamespacesWithSemanticLinks[NS_VOTES] = true;
 $smwgNamespacesWithSemanticLinks[NS_EVENTS] = true;
 
 wfLoadExtension( 'PageForms' );
+wfLoadExtension( 'InputBox' );
 
 require_once "$IP/extensions/SemanticRating/SemanticRating.php";
 
