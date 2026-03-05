e3ef00702cdecc131ba3b4b50a98d3c12b9b55fc
hcooper <coops@fawk.eu>
2026-03-04 16:55:26 -0800

    fix bash helpers for MW 1.35 (#166)
    * fix helpers for 1.35
    
    * Add Topo namespace to tools

--- rw_helpers.sh
+++ rw_helpers.sh
@@ -33,12 +33,16 @@ denter () {
 # Execute a command in a container
 dexec () {
   get_container $1
+  local saved_state=$(stty -g 2>/dev/null)
   docker exec -it $container "${@:2}"
+  local exit_code=$?
+  [[ -n "$saved_state" ]] && stty "$saved_state" 2>/dev/null || stty sane 2>/dev/null
+  return $exit_code
 }
 
 # Execute a database query
 dbquery () {
-  echo $@ >&2
+  [[ -n "$RW_VERBOSE" ]] && echo $@ >&2
   dexec db mysql -uroot -p"$RW_ROOT_DB_PASSWORD" "$@"
 }
 
@@ -61,55 +65,58 @@ findapage() {
 }
 
 # Search for page content (if no search query provided returns all page names)
-# TODO: patch query post MW 1.35 upgrade
 findinpage() {
-  dbquery "-N" "-s" "-e use ropewiki;
-
-  SELECT
-    CASE page_namespace
-      WHEN 0 THEN page_title
-      WHEN 1 THEN CONCAT('Talk:', page_title)
-      WHEN 2 THEN CONCAT('User:', page_title)
-      WHEN 3 THEN CONCAT('User talk:', page_title)
-      WHEN 4 THEN CONCAT('Project:', page_title)
-      WHEN 5 THEN CONCAT('Project talk:', page_title)
-      WHEN 6 THEN CONCAT('File:', page_title)
-      WHEN 7 THEN CONCAT('File talk:', page_title)
-      WHEN 8 THEN CONCAT('MediaWiki:', page_title)
-      WHEN 9 THEN CONCAT('MediaWiki talk:', page_title)
-      WHEN 10 THEN CONCAT('Template:', page_title)
-      WHEN 11 THEN CONCAT('Template talk:', page_title)
-      WHEN 12 THEN CONCAT('Help:', page_title)
-      WHEN 13 THEN CONCAT('Help talk:', page_title)
-      WHEN 14 THEN CONCAT('Category:', page_title)
-      WHEN 15 THEN CONCAT('Category talk:', page_title)
-      WHEN 102 THEN CONCAT('Property:', page_title)
-      WHEN 103 THEN CONCAT('Property talk:', page_title)
-      WHEN 106 THEN CONCAT('Form:', page_title)
-      WHEN 107 THEN CONCAT('Form talk:', page_title)
-      WHEN 108 THEN CONCAT('Concept:', page_title)
-      WHEN 109 THEN CONCAT('Concept talk:', page_title)
-      WHEN 170 THEN CONCAT('Filter:', page_title)
-      WHEN 171 THEN CONCAT('Filter talk:', page_title)
-      WHEN 190 THEN CONCAT('Conditions:', page_title)
-      WHEN 191 THEN CONCAT('Conditions talk:', page_title)
-      WHEN 192 THEN CONCAT('Preparation:', page_title)
-      WHEN 193 THEN CONCAT('Preparation talk:', page_title)
-      WHEN 194 THEN CONCAT('Incidents:', page_title)
-      WHEN 195 THEN CONCAT('Incidents talk:', page_title)
-      WHEN 196 THEN CONCAT('Votes:', page_title)
-      WHEN 197 THEN CONCAT('Votes talk:', page_title)
-      WHEN 198 THEN CONCAT('Events:', page_title)
-      WHEN 199 THEN CONCAT('Events talk:', page_title)
-      WHEN 200 THEN CONCAT('Lists:', page_title)
-      WHEN 201 THEN CONCAT('Lists talk:', page_title)
-      WHEN 828 THEN CONCAT('Module:', page_title)
-      WHEN 829 THEN CONCAT('Module talk:', page_title)
-      ELSE CONCAT('NS', page_namespace, ':', page_title)
-    END AS full_title
-  FROM page
-  JOIN revision ON page.page_latest = revision.rev_id
-  JOIN text ON revision.rev_text_id = text.old_id
-  WHERE text.old_text LIKE '%$@%';" | grep -sv "interface can be insecure"
+  dbquery "-N" "-s" "-e" "use ropewiki;
+SELECT
+  CASE page_namespace
+    WHEN 0 THEN REPLACE(page_title, '_', ' ')
+    WHEN 1 THEN CONCAT('Talk:', REPLACE(page_title, '_', ' '))
+    WHEN 2 THEN CONCAT('User:', REPLACE(page_title, '_', ' '))
+    WHEN 3 THEN CONCAT('User talk:', REPLACE(page_title, '_', ' '))
+    WHEN 4 THEN CONCAT('Project:', REPLACE(page_title, '_', ' '))
+    WHEN 5 THEN CONCAT('Project talk:', REPLACE(page_title, '_', ' '))
+    WHEN 6 THEN CONCAT('File:', REPLACE(page_title, '_', ' '))
+    WHEN 7 THEN CONCAT('File talk:', REPLACE(page_title, '_', ' '))
+    WHEN 8 THEN CONCAT('MediaWiki:', REPLACE(page_title, '_', ' '))
+    WHEN 9 THEN CONCAT('MediaWiki talk:', REPLACE(page_title, '_', ' '))
+    WHEN 10 THEN CONCAT('Template:', REPLACE(page_title, '_', ' '))
+    WHEN 11 THEN CONCAT('Template talk:', REPLACE(page_title, '_', ' '))
+    WHEN 12 THEN CONCAT('Help:', REPLACE(page_title, '_', ' '))
+    WHEN 13 THEN CONCAT('Help talk:', REPLACE(page_title, '_', ' '))
+    WHEN 14 THEN CONCAT('Category:', REPLACE(page_title, '_', ' '))
+    WHEN 15 THEN CONCAT('Category talk:', REPLACE(page_title, '_', ' '))
+    WHEN 102 THEN CONCAT('Property:', REPLACE(page_title, '_', ' '))
+    WHEN 103 THEN CONCAT('Property talk:', REPLACE(page_title, '_', ' '))
+    WHEN 106 THEN CONCAT('Form:', REPLACE(page_title, '_', ' '))
+    WHEN 107 THEN CONCAT('Form talk:', REPLACE(page_title, '_', ' '))
+    WHEN 108 THEN CONCAT('Concept:', REPLACE(page_title, '_', ' '))
+    WHEN 109 THEN CONCAT('Concept talk:', REPLACE(page_title, '_', ' '))
+    WHEN 170 THEN CONCAT('Filter:', REPLACE(page_title, '_', ' '))
+    WHEN 171 THEN CONCAT('Filter talk:', REPLACE(page_title, '_', ' '))
+    WHEN 190 THEN CONCAT('Conditions:', REPLACE(page_title, '_', ' '))
+    WHEN 191 THEN CONCAT('Conditions talk:', REPLACE(page_title, '_', ' '))
+    WHEN 192 THEN CONCAT('Preparation:', REPLACE(page_title, '_', ' '))
+    WHEN 193 THEN CONCAT('Preparation talk:', REPLACE(page_title, '_', ' '))
+    WHEN 194 THEN CONCAT('Incidents:', REPLACE(page_title, '_', ' '))
+    WHEN 195 THEN CONCAT('Incidents talk:', REPLACE(page_title, '_', ' '))
+    WHEN 196 THEN CONCAT('Votes:', REPLACE(page_title, '_', ' '))
+    WHEN 197 THEN CONCAT('Votes talk:', REPLACE(page_title, '_', ' '))
+    WHEN 198 THEN CONCAT('Events:', REPLACE(page_title, '_', ' '))
+    WHEN 199 THEN CONCAT('Events talk:', REPLACE(page_title, '_', ' '))
+    WHEN 200 THEN CONCAT('Lists:', REPLACE(page_title, '_', ' '))
+    WHEN 201 THEN CONCAT('Lists talk:', REPLACE(page_title, '_', ' '))
+    WHEN 828 THEN CONCAT('Module:', REPLACE(page_title, '_', ' '))
+    WHEN 829 THEN CONCAT('Module talk:', REPLACE(page_title, '_', ' '))
+    WHEN 3000 THEN CONCAT('Topo:', REPLACE(page_title, '_', ' '))
+    WHEN 3001 THEN CONCAT('Topo talk:', REPLACE(page_title, '_', ' '))
+    ELSE CONCAT('NS', page_namespace, ':', REPLACE(page_title, '_', ' '))
+  END AS full_title
+FROM page
+JOIN revision ON page.page_latest = revision.rev_id
+JOIN slots ON slots.slot_revision_id = revision.rev_id
+JOIN slot_roles ON slots.slot_role_id = slot_roles.role_id AND slot_roles.role_name = 'main'
+JOIN content ON slots.slot_content_id = content.content_id
+JOIN text ON LEFT(content.content_address,3) = 'tt:' AND text.old_id = CAST(SUBSTRING(content.content_address,4) AS UNSIGNED)
+WHERE text.old_text LIKE '%$@%';" | grep -sv "interface can be insecure"
 }
 
