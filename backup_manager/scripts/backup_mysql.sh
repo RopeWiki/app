b3c3216ec13c14b034afd09fc2209d9498d31c76
hcooper <coops@fawk.eu>
2026-03-04 17:06:12 -0800

    Backup manager improvements & fixes (#170)
    * mysql:5.7-debian container hacks
    
    * backupreader improvements

--- backup_manager/scripts/backup_mysql.sh
+++ backup_manager/scripts/backup_mysql.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "=== $(date) ==="
+# Verbose output is sent to STDERR. The successfully generated
+# filename is sent to STDOUT for programatic use.
+
+echo "=== $(date) ===" >&2
 
 # Backups can be disabled by touching `/do_not_backup_db`.
 if test -f "/do_not_backup_db"; then
-    echo "Backups disabled - found /do_not_backup_db"
+    echo "Backups disabled - found /do_not_backup_db" >&2
     exit
 fi
 
 BASE_PATH="/home/backupreader/backups"
 DATE=$(date +'%Y-%m-%d-%H%M%S')  # e.g. 2023-05-13-181803
-BACKUP="${BASE_PATH}/all-backup-${DATE}.sql.zst"
+TMP_BACKUP="${BASE_PATH}/tmp_all-backup-${DATE}.sql.zst"
+FINAL_BACKUP="${BASE_PATH}/all-backup-${DATE}.sql.zst"
 
-echo "Starting backup to $BACKUP"
+echo "Starting backup to $FINAL_BACKUP" >&2
 
-time mysqldump --host ropewiki_db \
+{ time mysqldump --host ropewiki_db \
           --all-databases \
           --add-drop-database \
           --single-transaction \
           --user=root --password="{{RW_ROOT_DB_PASSWORD}}" \
-          | zstd -10 > ${BACKUP}
+          | zstd -10 > ${TMP_BACKUP}; } 2>&2
+
+mv ${TMP_BACKUP} ${FINAL_BACKUP}
 
 # Owner: rw-, Group: r--, Others: r--
-chmod 644 ${BACKUP}
+chmod 644 ${FINAL_BACKUP}
 
-ls -lah ${BACKUP}
+ls -lah ${FINAL_BACKUP} >&2
 
 touch $BASE_PATH/last_success
 
-echo "==============="
+echo "===============" >&2
+
+echo -n "${FINAL_BACKUP}"
